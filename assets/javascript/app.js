025cfcf06c25273e08282b4f723f91446007d388
Jessica Lin <jessicalin@Jessicas-MacBook-Pro.local>
2017-08-28 15:20:01 -0400

    new score

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,12 @@
-
-//JQUERY FORM 
-
-$( "form" ).submit(function( event ) {
-  if ( $( "input:first" ).val() === "s" ) {
-    $( "span" ).text( "Question 01. Marble is primarly white but can come in other colors as well" ).show();
-    return;
-  }
  
-  $( "span" ).text( "Fine...go play someone else's game" ).show();
-  event.preventDefault();
-});
-
-//end of form 
 
 var questions = {
 
-	q1: ["Marble is primarily white but can come in other colors as well.", "t"],
-	q2: ["Back in ancient times, you could easily tell where a piece of marble came from by looking at its color.", "t"],
-	q3: ["Marble has been used to make statues and provide flooring since ancient times.", "t"],
-	q4: ["The Empire States in New York is made entirely of marble", "f"],
-	q5: ["Marble is a type of foliated metamorphic rock, which are rocks that can be modified by heat, pressire, and chemical processes", "f"]
+	q1: ["Q1. Marble is primarily white but can come in other colors as well.", "t"],
+	q2: ["Q2. Back in ancient times, you could easily tell where a piece of marble came from by looking at its color.", "t"],
+	q3: ["Q3. Marble has been used to make statues and provide flooring since ancient times.", "t"],
+	q4: ["Q4. The Empire States in New York is made entirely of marble", "f"],
+	q5: ["Q5. Marble is a type of foliated metamorphic rock, which are rocks that can be modified by heat, pressire, and chemical processes", "f"]
 }
 
 var score = 0;
@@ -30,17 +17,7 @@ var questionsArray = [questions.q1, questions.q2, questions.q3, questions.q4, qu
 
 //RENDERING QUESTIONS 
 
-function renderQuestion() {
 
-	if (questionIndex <= (questionsArray.length - 1)) {
-          document.querySelector("#questions").innerHTML = questionsArray[questionIndex][0];
-        }
-
-	else {
-		document.querySelector("#questions").innerHTML = "Game Over!";
-         document.querySelector("#score").innerHTML = "Final Score: " + score + " out of " + questionsArray.length;
-	}
-}
 
 
 //UPDATE SCORE
@@ -51,54 +28,177 @@ function updateScore() {
 
 //FUNCTIONS
 
-renderQuestion();
+
 updateScore();
 
-document.onkeyup = function(event) {
 
-	if(questionIndex === questionsArray.lenght){
+// STOPWATCH ACTIVITY (SOLUTION)
+// =============================
 
-		return;
-	}
+// This code will run as soon as the page loads
+window.onload = function() {
+
+  $("#reset").on("click", stopwatch.reset);
+  $("#start").on("click", stopwatch.start);
+  $("#restart").hide();
 
 
-	var userInput = String.fromCharCode(event.keyCode).toLowerCase();
+  $("#instructions").text("Instructions: Type 'T' for true or'F' for false.");
+  $("#instructions2").text("How to win: Answer the question within 15 seconds OR finish all the questions with 4 out of 5 correct answers");
 
-
-	if (userInput === "t" || userInput === "f") {
-
-		//insert change of score here
-		score++;
-		updateScore()
-	}
-
-	else {
-
-		//update to be incorrect
-	}
-
-		//call function and increment questionindex
-	questionIndex++;
-	renderQuestion();
-}
-
-//PROGRESS BAR
-
-function progress(timeleft, timetotal, $element) {
-    var progressBarWidth = timeleft * $element.width() / timetotal;
-    $element
-        .find('div')
-        .animate({ width: progressBarWidth }, 500)
-        .html(timeleft + " seconds to go");
-    if(timeleft > 0) {
-        setTimeout(function() {
-            progress(timeleft - 1, timetotal, $element);
-        }, 1000);
-    }
 };
 
-progress(180, 180, $('#progressBar'));
-//PROGRESS BAR
+//  Variable that will hold our setInterval that runs the stopwatch
+var intervalId;
+
+//prevents the clock from being sped up unnecessarily
+var clockRunning = false;
+
+// Our stopwatch object
+var stopwatch = {
+
+  time: 15,
+  
+
+  restart: function() {
+
+//     stopwatch.time = 0;
+//     questionsArray = [];
+//     score = 0;
+
+//     function updateScore() {
+//   document.querySelector("#score").innerHTML = "Score: " + score;
+// }
+
+
+  //   $("#display").html("5");
+
+   },
+
+
+
+  start: function() {
+
+    function fiveSeconds() {
+
+    stopwatch.time = 15;
+    questionIndex++;
+    renderQuestion();
+
+ }
+     $("#instructions").text("");
+     $("#instructions2").text("");
+     $("#start").hide();
+
+  
+    renderQuestion();
+    setInterval(fiveSeconds, 1000 * 15);
+
+    // DONE: Use setInterval to start the count here and set the clock to running.
+    if (!clockRunning) {
+        intervalId = setInterval(stopwatch.count, 1000);
+        clockRunning = true;
+          //QUESTIONS ONKEYUP
+
+                  document.onkeyup = function(event) {
+
+            if(questionIndex === questionsArray.lenght){
+
+              return;
+            }
+
+
+            //USER INPUT
+            var userInput = String.fromCharCode(event.keyCode).toLowerCase();
+
+
+            if (userInput === "t" || userInput === "f") {
+            
+              stopwatch.time = 15;
+
+              if (userInput === questionsArray[questionIndex][1]) {
+              $("#results").html("You are correct!");
+              $("#wrongResults").html("");
+              score++;
+               updateScore();
+          }
+
+            else {
+              $("#results").html("");
+              $("#wrongResults").html("You are incorrect :(");
+              //update to be incorrect
+            }
+
+              //call function and increment questionindex
+            questionIndex++;
+            renderQuestion();
+          }
+        }
+      }
+
+function renderQuestion() {
+
+  if (questionIndex <= (questionsArray.length - 1)) {
+          document.querySelector("#questions").innerHTML = questionsArray[questionIndex][0];
+        }
+
+  else {
+    clearInterval(intervalId);
+    clockRunning = false;
+
+     if(score >= 4) {
+
+    document.querySelector("#questions").innerHTML = "Horray!!";
+    $("#wrongResults").html("");
+    $("#results").html("");
+    $("#restart").show();
+     }
+
+     else {
+
+    document.querySelector("#questions").innerHTML = "Better luck next time.";
+    $("#wrongResults").html("");
+    $("#results").html("");
+    $("#restart").show();
+     }
+
+    document.querySelector("#score").innerHTML = "Final Score: " + score + " out of " + questionsArray.length;
+  }
+}
+
+          //END OF ONKEYUP
+
+  
+  },
+
+  count: function() {
+
+  
+    stopwatch.time--;
+
+    if(stopwatch.time) {
+
+
+    $('.progress-bar').css('width', stopwatch.time + '%');
+
+     console.log(stopwatch.time);
+
+  }
+  
+   
+    var converted = stopwatch.time;
+  
+    $("#display").html(converted);
+  },
+
+
+};
+
+
+
+
+
+
 
 
 
